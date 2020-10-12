b641d9ba84a0f5981eb5a11724f3638e020b0947
Victor Rocha <victordsrocha@gmail.com>
2020-10-12 19:14:16 -0300

    1

---
+++ Matrizes3D/Assets/Scripts/Main.cs
@@ -0,0 +1,279 @@
+﻿/*
+ * Esta classe executa exemplos de uso de Mat4
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    // instância de Mat4
+    private Mat4 _matrizMat4;
+
+    // Utilizei para os testes uma esfera contendo somente o "vértice" de seu centro e um cubo com seus oito vértices
+    // Os valores dos vértices do cubo são relativos a posição no espaço do próprio cubo
+    private Mesh _cuboMesh;
+    private GameObject _cubo;
+    private GameObject _esfera;
+
+    // inicialização UI
+    public Button botaoRotacaoX;
+    public Button botaoRotacaoY;
+    public Button botaoRotacaoZ;
+    public Button botaoTranslacao;
+    public Button botaoEscala;
+    public Button botaoResetar;
+    public Button botaoRotacaoArbitrario1;
+    public Button botaoRotacaoArbitrario2;
+    public Button botaoTRS;
+
+    // controle de operação
+    private int _op = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // inicialização UI
+        InicializarBotoes();
+
+        // inicialização Mat4
+        _matrizMat4 = new Mat4();
+
+        // inicialização esfera e cubo
+        _cubo = GameObject.FindGameObjectWithTag("Cubo");
+        _cuboMesh = _cubo.GetComponent<MeshFilter>().mesh;
+        _esfera = GameObject.FindGameObjectWithTag("Esfera");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ExecutarOperacaoExemplo();
+    }
+
+
+    void ExemploRotacionarEixoX()
+    {
+        _matrizMat4.RotacaoEixoX(Mathf.PI * 0.01f);
+        _esfera.transform.position = _matrizMat4.Transformar(_esfera.transform.position);
+
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    void AtivarExemploRotacionarX()
+    {
+        _op = 1;
+    }
+
+    void ExemploRotacionarEixoY()
+    {
+        _matrizMat4.RotacaoEixoY(Mathf.PI * 0.01f);
+        _esfera.transform.position = _matrizMat4.Transformar(_esfera.transform.position);
+
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    void AtivarExemploRotacionarY()
+    {
+        _op = 2;
+    }
+
+    void ExemploRotacionarEixoZ()
+    {
+        _matrizMat4.RotacaoEixoZ(Mathf.PI * 0.01f);
+        _esfera.transform.position = _matrizMat4.Transformar(_esfera.transform.position);
+
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    void AtivarExemploRotacionarZ()
+    {
+        _op = 3;
+    }
+
+
+    void ExemploTranslacao()
+    {
+        _matrizMat4.Translacao(0.02f, 0.02f, 0.02f);
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    void AtivarExemploTranslacao()
+    {
+        _op = 4;
+    }
+
+    void ExemploEscala()
+    {
+        _matrizMat4.Escala(1.01f, 1.01f, 1.01f);
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    void AtivarExemploEscala()
+    {
+        _op = 5;
+    }
+
+    void ExemploRotacionarEixoArbitrario1()
+    {
+        Vector3 u = new Vector3(2, 2, 2);
+        _matrizMat4.RotacaoEixoArbitrario(Mathf.PI * 0.01f, u);
+        _esfera.transform.position = _matrizMat4.Transformar(_esfera.transform.position);
+
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    public void AtivarExemploRotacionarArbitrario1()
+    {
+        _op = 6;
+    }
+
+    void ExemploRotacionarEixoArbitrario2()
+    {
+        Vector3 u = new Vector3(-4, 2, 5);
+        _matrizMat4.RotacaoEixoArbitrario(Mathf.PI * 0.01f, u);
+        _esfera.transform.position = _matrizMat4.Transformar(_esfera.transform.position);
+
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    public void AtivarExemploRotacionarArbitrario2()
+    {
+        _op = 7;
+    }
+
+    void ExemploTRS()
+    {
+        var u = new Vector3(0.1f, 0, -0.1f);
+        _matrizMat4.TRS(0.002f, 0.001f, 0.003f, Mathf.PI * 0.001f, u, 1.001f);
+        _esfera.transform.position = _matrizMat4.Transformar(_esfera.transform.position);
+
+        Vector3[] vertices = _cuboMesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = _matrizMat4.Transformar(vertices[i]);
+        }
+
+        _cuboMesh.vertices = vertices;
+    }
+
+    public void AtivarExemploTRS()
+    {
+        _op = 8;
+    }
+
+    void Resetar()
+    {
+        _op = 0;
+        _cubo.GetComponent<MeshGenerator>().CriarCubo();
+        _esfera.transform.position = new Vector3(3, 3, 0);
+    }
+
+    private void ExecutarOperacaoExemplo()
+    {
+        switch (_op)
+        {
+            case 1:
+                ExemploRotacionarEixoX();
+                break;
+            case 2:
+                ExemploRotacionarEixoY();
+                break;
+            case 3:
+                ExemploRotacionarEixoZ();
+                break;
+            case 4:
+                ExemploTranslacao();
+                break;
+            case 5:
+                ExemploEscala();
+                break;
+            case 6:
+                ExemploRotacionarEixoArbitrario1();
+                break;
+            case 7:
+                ExemploRotacionarEixoArbitrario2();
+                break;
+            case 8:
+                ExemploTRS();
+                break;
+        }
+    }
+
+    private void InicializarBotoes()
+    {
+        botaoRotacaoX.onClick = new Button.ButtonClickedEvent();
+        botaoRotacaoX.onClick.AddListener(AtivarExemploRotacionarX);
+        botaoRotacaoY.onClick = new Button.ButtonClickedEvent();
+        botaoRotacaoY.onClick.AddListener(AtivarExemploRotacionarY);
+        botaoRotacaoZ.onClick = new Button.ButtonClickedEvent();
+        botaoRotacaoZ.onClick.AddListener(AtivarExemploRotacionarZ);
+        botaoRotacaoArbitrario1.onClick = new Button.ButtonClickedEvent();
+        botaoRotacaoArbitrario1.onClick.AddListener(AtivarExemploRotacionarArbitrario1);
+        botaoRotacaoArbitrario2.onClick = new Button.ButtonClickedEvent();
+        botaoRotacaoArbitrario2.onClick.AddListener(AtivarExemploRotacionarArbitrario2);
+        botaoTranslacao.onClick = new Button.ButtonClickedEvent();
+        botaoTranslacao.onClick.AddListener(AtivarExemploTranslacao);
+        botaoEscala.onClick = new Button.ButtonClickedEvent();
+        botaoEscala.onClick.AddListener(AtivarExemploEscala);
+        botaoResetar.onClick = new Button.ButtonClickedEvent();
+        botaoResetar.onClick.AddListener(Resetar);
+        botaoTRS.onClick = new Button.ButtonClickedEvent();
+        botaoTRS.onClick.AddListener(AtivarExemploTRS);
+    }
+}
---
+++ Matrizes3D/Assets/Scripts/Mat4.cs
@@ -0,0 +1,233 @@
+﻿/*
+ * Fiz somente Matriz4x4 pois engloba o caso 2D
+ */
+
+using UnityEngine;
+
+public class Mat4
+{
+    private float[,] _m;
+
+    public Mat4()
+    {
+        Identidade();
+    }
+
+    public Vector3 Transformar(Vector3 v)
+    {
+        Vector3 u = new Vector3(
+            _m[0, 0] * v.x + _m[0, 1] * v.y + _m[0, 2] * v.z + _m[0, 3],
+            _m[1, 0] * v.x + _m[1, 1] * v.y + _m[1, 2] * v.z + _m[1, 3],
+            _m[2, 0] * v.x + _m[2, 1] * v.y + _m[2, 2] * v.z + _m[2, 3]
+        );
+
+        var uw = _m[3, 0] * v.x + _m[3, 1] * v.y + _m[3, 2] * v.z + _m[3, 3];
+        if (uw != 0)
+        {
+            u.x /= uw;
+            u.y /= uw;
+            u.z /= uw;
+        }
+
+        return u;
+    }
+
+
+    public void Translacao(float dx, float dy, float dz)
+    {
+        _m = new float[,]
+        {
+            {1, 0, 0, dx},
+            {0, 1, 0, dy},
+            {0, 0, 1, dz},
+            {0, 0, 0, 1}
+        };
+    }
+
+    public void Escala(float sx, float sy, float sz)
+    {
+        _m = new float[,]
+        {
+            {sx, 0, 0, 0},
+            {0, sy, 0, 0},
+            {0, 0, sz, 0},
+            {0, 0, 0, 1}
+        };
+    }
+
+    public void RotacaoEixoX(float theta)
+    {
+        _m = new float[,]
+        {
+            {1, 0, 0, 0},
+            {0, Mathf.Cos(theta), -Mathf.Sin(theta), 0},
+            {0, Mathf.Sin(theta), Mathf.Cos(theta), 0},
+            {0, 0, 0, 1}
+        };
+    }
+
+    public void RotacaoEixoY(float theta)
+    {
+        _m = new float[,]
+        {
+            {Mathf.Cos(theta), 0, Mathf.Sin(theta), 0},
+            {0, 1, 0, 0},
+            {-Mathf.Sin(theta), 0, Mathf.Cos(theta), 0},
+            {0, 0, 0, 1}
+        };
+    }
+
+    public void RotacaoEixoZ(float theta)
+    {
+        _m = new float[,]
+        {
+            {Mathf.Cos(theta), Mathf.Sin(theta), 0, 0},
+            {-Mathf.Sin(theta), Mathf.Cos(theta), 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}
+        };
+    }
+
+    public void RotacaoEixoArbitrario(float theta, Vector3 u)
+    {
+        // Substituição de u pelo versor de u
+        var magnitude = Mathf.Sqrt(u.x * u.x + u.y * u.y + u.z * u.z);
+        u.x /= magnitude;
+        u.y /= magnitude;
+        u.z /= magnitude;
+
+        var cos = Mathf.Cos(theta);
+        var sen = Mathf.Sin(theta);
+        var x = u.x;
+        var y = u.y;
+        var z = u.z;
+
+        _m = new float[,]
+        {
+            {
+                x * x * (1 - cos) + cos,
+                x * y * (1 - cos) - z * sen,
+                x * z * (1 - cos) + y * sen,
+                0
+            },
+
+            {
+                x * y * (1 - cos) + z * sen,
+                y * y * (1 - cos) + cos,
+                y * z * (1 - cos) - x * sen,
+                0
+            },
+
+            {
+                x * z * (1 - cos) - y * sen,
+                y * z * (1 - cos) + x * sen,
+                z * z * (1 - cos) + cos,
+                0
+            },
+
+            {
+                0,
+                0,
+                0,
+                1
+            }
+        };
+    }
+
+    public void TRS(float dx = 0, float dy = 0, float dz = 0,
+        float theta = 0, Vector3 u = new Vector3(),
+        float sx = 1, float sy = 1, float sz = 1)
+    {
+        var t = new Mat4();
+        t.Translacao(dx, dy, dz);
+
+        var r = new Mat4();
+        r.RotacaoEixoArbitrario(theta, u);
+
+        var s = new Mat4();
+        s.Escala(sx, sy, sz);
+
+        this._m = MultiplicarMatriz(this._m, s._m);
+        this._m = MultiplicarMatriz(this._m, r._m);
+        this._m = MultiplicarMatriz(this._m, t._m);
+    }
+
+    public void TRSinversa(float dx = 0, float dy = 0,
+        float dz = 0, float theta = 0, Vector3 u = new Vector3(),
+        float sx = 1, float sy = 1, float sz = 1)
+    {
+        var t = new Mat4();
+        t.Translacao(dx, dy, dz);
+        t._m = InverterMatriz(t._m);
+
+        var r = new Mat4();
+        r.RotacaoEixoArbitrario(theta, u);
+        r._m = InverterMatriz(r._m);
+
+        var s = new Mat4();
+        s.Escala(sx, sy, sz);
+        s._m = InverterMatriz(s._m);
+
+        this._m = MultiplicarMatriz(this._m, s._m);
+        this._m = MultiplicarMatriz(this._m, r._m);
+        this._m = MultiplicarMatriz(this._m, t._m);
+    }
+
+    private void Identidade()
+    {
+        _m = new float[,]
+        {
+            {1, 0, 0, 0},
+            {0, 1, 0, 0},
+            {0, 0, 1, 0},
+            {0, 0, 0, 1}
+        };
+    }
+
+    private float[,] InverterMatriz(float[,] matriz)
+    {
+        Matrix4x4 m4 = Matrix4x4.zero;
+
+        int c = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                m4[c] = matriz[i, j];
+                c++;
+            }
+        }
+
+        m4 = m4.inverse;
+
+        c = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                matriz[i, j] = m4[c];
+                c++;
+            }
+        }
+
+        return matriz;
+    }
+
+    private float[,] MultiplicarMatriz(float[,] matriz1, float[,] matriz2)
+    {
+        float[,] mult = new float[4, 4];
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    mult[i, j] += matriz1[i, k] * matriz2[k, j];
+                }
+            }
+        }
+
+        return mult;
+    }
+}
